18bb5095309018bc5b42a1edce2995f416e8e23c
adamnemecek <adamnemecek@gmail.com>
2021-10-16 08:47:53 -0700

    packaging

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
         )
         // .testTarget(
         //     name: "MyLibraryTests",
-        //     dependencies: ["MyLibrary"]
-            ),
+        //     dependencies: ["MyLibrary"])
+        // ,
     ]
 )
