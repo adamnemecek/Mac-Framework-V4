78ceca3c938e1c21653bb8d5ba0a502134dbc679
adamnemecek <adamnemecek@gmail.com>
2022-06-20 18:19:22 -0700

    updated

--- build.sh
+++ build.sh
@@ -1,5 +1,1 @@
 xcodebuild -create-xcframework -framework Paddle.framework -output Paddle.xcframework
-# git add Paddle.xcframework
-# git touch Package.swift 
-# copy the contents from my Package.swift (https://github.com/adamnemecek/Mac-Framework-V4/blob/master/Package.swift, consider changing some things as you see fit). 
-# git commit -am 'added SPM Support'
